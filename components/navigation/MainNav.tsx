a1fea7e6ca3659864c623647ceef4d8fab9b7cdb
Brian <brian@burcapital.com>
2025-12-18 12:47:15 +0100

    login

--- components/navigation/MainNav.tsx
+++ components/navigation/MainNav.tsx
@@ -1,15 +1,17 @@
-import Link from 'next/link';
+﻿import Link from 'next/link';
 import { auth, signOut } from '@/lib/auth';
 
 export default async function MainNav() {
   const session = await auth();
+  const prefetch = !!session;
+
   return (
     <nav className="border-b mb-8">
       <div className="container mx-auto flex gap-6 py-4">
-        <Link href="/dashboard" className="font-semibold">Dashboard</Link>
-        <Link href="/contacts">Contacts</Link>
-        <Link href="/campaigns">Campaigns</Link>
-        <Link href="/groups">Groups</Link>
+        <Link href="/dashboard" prefetch={prefetch} className="font-semibold">Dashboard</Link>
+        <Link href="/contacts" prefetch={prefetch}>Contacts</Link>
+        <Link href="/campaigns" prefetch={prefetch}>Campaigns</Link>
+        <Link href="/groups" prefetch={prefetch}>Groups</Link>
         {!!session && (
           <form className="ml-auto"
             action={async () => {
